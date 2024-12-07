592b92c563b8364f7d9bbf39ddb1f2b63234e717
ParizahShaikhh <137597244+ParizahShaikhh@users.noreply.github.com>
2024-12-07 20:11:38 +0500

    initial update

---
+++ 2nd-hackathon-q2/src/app/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+import FooterPage from "./components/Footer";
+import HeaderPage from "./components/Header";
+
+export default function Home() {
+  return (
+   <>
+   <HeaderPage />
+   <FooterPage />
+   </>
+  );
+}
